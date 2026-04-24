53a4bd6ad28c7724ac7084b92ed65aea3f0d1de0
Abudan <abudan.royale18@gmail.com>
2026-04-24 20:54:51 +0700

    feat: build AI Assistant page with smart navbar and pixel-perfect chat layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import HeroSection from '../components/HeroSection';
 import AiAssistantSection from '../components/AiAssistantSection';
 import InsightSection from '../components/InsightSection';
 import FooterSection from '../components/FooterSection';
+import PreviewSection from '../components/PreviewSection';
 
 const Home = () => {
   return (
     <div className="w-full min-h-screen">
       <NavbarSection />
       <HeroSection />
+      <PreviewSection />
       <AiAssistantSection />
       <InsightSection />
       <FooterSection />

636388e9edee87c108d3f3ffe219762f1b0efc9a
Abudan <abudan.royale18@gmail.com>
2026-04-28 21:43:26 +0700

    style: fix responsive layout and positioning for insight floating charts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AIAssistantPage from './pages/AIAssistantPage';
+import InsightPage from './pages/InsightPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ai-assistant" element={<AIAssistantPage />} />
+        <Route path="/insights" element={<InsightPage />} />
       </Routes>
     </Router>
   );
